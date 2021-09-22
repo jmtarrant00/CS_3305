e8c0489618e7fd0c26d88d849da5760858390019
Starwolf16 <jmtarrant00@gmail.com>
2021-09-22 12:59:47 -0400

    Desktop

--- CS_3305/A3_Node1/node1_Test.cpp
+++ CS_3305/A3_Node1/node1_Test.cpp
@@ -17,10 +17,21 @@ int main() {
 }
 
 void printMenu(){
+	cout << "------------- MAIN MENU ------------------" << endl;
 	cout << "1: Test Function std::size_list_length" << endl;
 	cout << "2: Test Function void list_head_insert" << endl;
 	cout << "3: Test Function void list_insert" << endl;
 	cout << "4: Test Function node *list_search" << endl;
-	
+	cout << "5: Test Fuction const node *list_search" << endl;
+	cout << "6: Test Function node *list_locate" << endl;
+	cout << "7: Test Function const node *list_locate" << endl;
+	cout << "8: Test Function void list_head_remove" << endl;
+	cout << "9: Test Function void list_remove" << endl;
+	cout << "10: Test Function void list_clear" << endl;
+	cout << "11: Test Function void list_copy" << endl;
+	cout << "12: Display List" << endl;
+	cout << "12: Exit Program" << endl;
+	cout << '\n';
+	cout << "Enter option number" << endl;
 }
 
