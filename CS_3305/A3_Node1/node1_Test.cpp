08ab1da910d1422bdb57a62306753885bf88851a
Starwolf16 <jmtarrant00@gmail.com>
2021-09-22 12:27:41 -0400

    Laptop work 09/21/21

--- CS_3305/A3_Node1/node1_Test.cpp
+++ CS_3305/A3_Node1/node1_Test.cpp
@@ -4,3 +4,23 @@
 // Instructor: Dr. Haddad
 // Assignment: 3
 
+#include <cstdlib>
+#include <String>
+#include <iostream>
+#include <cassert> // Provides assert
+#include "node1.h" 
+
+using namespace std;
+
+int main() {
+	
+}
+
+void printMenu(){
+	cout << "1: Test Function std::size_list_length" << endl;
+	cout << "2: Test Function void list_head_insert" << endl;
+	cout << "3: Test Function void list_insert" << endl;
+	cout << "4: Test Function node *list_search" << endl;
+	
+}
+
