9bc644c486e6058bd7ed8a2e57104bf887ec54a8
Starwolf16 <jmtarrant00@gmail.com>
2021-10-20 23:10:07 -0400

    Commenting code

--- CS_3305/A5_Average/AverageGrades.cpp
+++ CS_3305/A5_Average/AverageGrades.cpp
@@ -4,25 +4,39 @@
 // Instructor: Dr. Haddad
 // Assignment: 5
 
-#include <cstdlib>
-#include <iostream>
-#include <string>
-#include <limits>
+#include <cstdlib>  // provides EXIT_SUCCESS and std library
+#include <iostream> // provides cout and cin
 
-using namespace std;
+using namespace std; // allows all std lib items to be used
 
+//This function prints the menu that is used throughout the program
+// it also prompts the user to enter an option
 void printMenu();
+
+// This function gets the class size from the user
+// It also checks to see if the class size is negative
+// but it allows the user to enter 0 as a size
 int getClassSize();
-int* getGrades(classSize);
-int findAverage(int*& grades[], int classSize); 
+
+// This function gets the grade of the class from the user
+// It stores the grades in a dynamic array, and returns a pointer 
+// to the main function 
+int* getGrades(int classSize);
+
+// This function recursively calculates the average grade of the class
+// The first parameter is the grades array. The second parameter is a counter
+// and the third parameter is the size of the class
+int findAverage(int grades[], int i, int classSize); 
 
 int main() {
-	int choice = 0;
-	int classSize;
-	int* grades[];
-	bool choicePass = false;
+	int choice = 0; 		 // initializes choice to 0
+	int classSize; 			 // stores the size of the class for both the get grades and average function
+	int *grades; 			 // pointer to store the grades to pass to the average function
+	bool choicePass = false; // used to exit the do while that checks the menu input
 	do {
-		do {
+		do { 
+			// This do while loop checks the value input for the menu options. It won't move on until choicePass is true
+			// which only happens when the user enters a valid option
 			printMenu();
 			cin >> choice;
 			if (choice >= 1 && choice <= 4) {
@@ -34,21 +48,29 @@ int main() {
 			}
 		} while (choicePass == false);
 		switch(choice) {
+			// This switch statement is what determines what is executed depending on what the user entered
 			case 1:
-				classSize = getClassSize();
+				classSize = getClassSize(); // sets class size 
 				break;
-			case 2:
-				
+			case 2: {
+				grades = getGrades(classSize); // sets the grades
+				break;
+			}
 			case 3:
-				
+				cout << "Finding the Average... " << endl;
+				cout << "The average grade is: " << findAverage(grades, 0, classSize) << endl; // computes and outputs the average grade
+				break;
 			case 4: 
 				cout << "Exiting Program..." << endl;
 				break;
 		}
 	} while (choice != 4);
+	return EXIT_SUCCESS;
 }
 
 void printMenu(){
+	// Precondition: none
+	// Postcondition: prints the main menu for the user
 	cout << endl;
 	cout << "-----MAIN MENU-----" << endl;
 	cout << "1: Read Class Size" << endl;
@@ -60,9 +82,13 @@ void printMenu(){
 }
 
 int getClassSize(){
+	// Precondition: None
+	// Postcondition: takes input for the number of students in the class
+	// and checks to make sure the class size is >= 0
 	int classSize;
 	cout << "Please enter the class size: ";
 	do {
+		// Takes input for size and checks if it's >= 0
 		cin >> classSize;
 		if (classSize < 0) {
 			cout << "Please enter a size greater than or equal to 0." << endl;
@@ -72,16 +98,49 @@ int getClassSize(){
 	return classSize;
 }
 
-int* getGrades(classSize) {
-	int* grades[] = new int[classSize];
+int* getGrades(int classSize) {
+	// Precondition: classSize is non-negative
+	// Postcondition: returns pointer to an array that contains 
+	// the grades for the class
+	int* gradesInput = new int[classSize];
 	for (int i = 0; i < classSize; i++) {
-		cout << "What is the grade? ";
-		cin >> grades[i];
-		
+		switch(i + 1){
+			// switches the output based on the index of the grade being entered
+			case 1:
+				cout << "What is the 1st grade? ";
+				cin >> gradesInput[i];
+				break;
+			case 2:
+				cout << "What is the 2nd grade? ";
+				cin >> gradesInput[i];
+				break;
+			case 3: 
+				cout << "What is the 3rd grade? ";
+				cin >> gradesInput[i];
+				break;
+			default:
+				cout << "What is the " << i + 1 << "th grade? ";
+				cin >> gradesInput[i];
+				break;
+		}
 	}
+	return gradesInput;
 	
 }
 
-int* findAverage(classSize) {
+int findAverage(int grades[], int i, int classSize) {
+	// Precondition: grades[] has elements, i and classSize are non-negative
+	// Postcondition: returns the average grade of the class
 	
+	// if the class size is 1, return the first and only element in the array
+	if (i == classSize - 1) {
+		return grades[i];
+	}
+	
+	// base case: if i is 
+	if (i == 0) {
+		return ((grades[i] + findAverage(grades, i + 1, classSize)) / classSize);
+	}
+	
+	return (grades[i] + findAverage(grades, i + 1, classSize));
 }
