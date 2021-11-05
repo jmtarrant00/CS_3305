bb7097a76ec8751d352b7b61746a93d78199979b
Starwolf16 <jmtarrant00@gmail.com>
2021-11-05 15:49:34 -0400

    Finish pt. 1

--- CS_3305/A6_BST/myTestBST.cpp
+++ CS_3305/A6_BST/myTestBST.cpp
@@ -13,40 +13,46 @@
 
 using namespace std;
 
-binary_tree_node buildTree(item *words, int size);
+template<class item>
+binary_tree_node<item> *buildTree(item *words, int low, int high);
 
 int main(){
 	int size = 0;
+	binary_tree_node<string> *node;
 	string words[31] {"After", "Also", "any", "back", "because", "come", "day", "even", 
 					  "first", "give", "how", "its", "look", "most", "new", "now", 
 					  "only", "other", "our", "over", "than", "then", "these", "think", 
 					  "two", "us", "use", "want", "way", "well", "work"};
 							 
 	size = (sizeof(words) / sizeof(words[0]));
-	buildTree(words, size);
+	node = buildTree(words, 0,  size);
+	
+	cout << "================= Build Tree =================" << endl;
+	cout << endl << node << endl << endl;
+	print(node, 0);
+	cout << endl << "Balanced? " << check_balanced(node) << endl;
+	
+	cout << cout << "================= Remove Root =================" << endl;
+	
+	
 	
 	return EXIT_SUCCESS;
 }
 
-template<class item> item buildTree(item *words, int size) {
-	binary_search_tree<item> myBST;
-	int mid = size / 2;
-	
-	item left[mid];
-	item right[mid];
-	
-	if 
-	
-	item arrMid = words[mid];
-	myBST.insert(arrMid);
-	for (int i = 0; i < mid; i++){
-		right[i] = words[i];
+template<class item>
+binary_tree_node<item> *buildTree(item *words, int low, int high) {
+	if (low > high) {
+		return NULL;
 	}
 	
-	for (int j = (size + 1); j < size; j++) {
-		left[j] = words[j];
-	}
+	int mid = (low + high) / 2;
 	
-	buildTree(right, mid);
-	buildTree(left, mid);
+	binary_tree_node<item> *node = new binary_tree_node<item>(words[mid]);
+	
+	node -> set_left(buildTree(words, low, mid - 1));
+	
+	node -> set_right(buildTree(words, mid + 1, high));
+	
+	return node;
+	
 }
