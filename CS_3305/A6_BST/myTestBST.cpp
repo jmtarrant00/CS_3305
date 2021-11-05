ad38f6bf7ad6cf8fe38ea5841c806d3a6391f410
Starwolf16 <jmtarrant00@gmail.com>
2021-11-05 19:49:10 -0400

    Code Comments

--- CS_3305/A6_BST/myTestBST.cpp
+++ CS_3305/A6_BST/myTestBST.cpp
@@ -4,15 +4,18 @@
 // Instructor: Dr. Haddad
 // Assignment: 6
 
-#include <cstdlib>
-#include <cassert>
-#include <string>
-#include <iostream>
-#include <array>
-#include "bst.h"
+#include <cstdlib>	// provides the std lib
+#include <string>	// provides the string type
+#include <iostream> // Provides cout
+#include "bst.h"	// includes bst and bintree header files
 
-using namespace std;
+using namespace std; // allows for all std lib items to be used
 
+// This function takes an array of words, the lowest index and the highest index
+// and recursively creates a binary search tree from the array. 
+// It splits the array into two parts and those two parts make up 
+// the left and right child of the BST
+// It returns a pointer to the root node of thee
 template<class item>
 binary_tree_node<item> *buildTree(item *words, int low, int high);
 
@@ -24,37 +27,36 @@ int main(){
 					  "only", "other", "our", "over", "than", "then", "these", "think", 
 					  "two", "us", "use", "want", "way", "well", "work"};
 							 
-	size = (sizeof(words) / sizeof(words[0]));
-	node = buildTree(words, 0,  size);
+	size = (sizeof(words) / sizeof(words[0])); // calculates the size of the array
+	node = buildTree(words, 0,  size); // starts build tree function 
 	
 	cout << "================= Build Tree =================" << endl;
 	cout << endl << node << endl << endl;
-	print(node, 0);
-	cout << endl << "Balanced? " << check_balanced(node) << endl;
+	print(node, 0); // prints out the BST in tree form
+	cout << endl << "Balanced? " << check_balanced(node) << endl; // Checks for tree balance and outputs the depth of the tree if balanced
 	
 	cout << "================= Remove Root =================" << endl;
-	string data = node -> data();
-	cout << "Remove: " << words[size / 2] << endl;
-	bst_remove(node, data);
-	print(node, 0);	
+	string data = node -> data(); // Gets the word of the root node
+	cout << "Remove: " << data << endl; // prints out the word to be removed
+	bst_remove(node, data); // calls the remove function, which then reorganizes the tree
+	print(node, 0);	// prints out the new tree
 	
 	return EXIT_SUCCESS;
 }
 
 template<class item>
 binary_tree_node<item> *buildTree(item *words, int low, int high) {
-	if (low > high) {
+	/** BASE CASE */
+	if (low > high) { // checks to see if the upper bound is smaller than the lower bound
 		return NULL;
 	}
 	
-	int mid = (low + high) / 2;
+	int mid = (low + high) / 2; // finds the index of the middle of the array
+	binary_tree_node<item> *node = new binary_tree_node<item>(words[mid]); // creates a new node from the word in the middle of the array
 	
-	binary_tree_node<item> *node = new binary_tree_node<item>(words[mid]);
+	node -> set_left(buildTree(words, low, mid - 1)); // recursive call for the left child of the node
+	node -> set_right(buildTree(words, mid + 1, high)); // recursive call for the right child of the node
 	
-	node -> set_left(buildTree(words, low, mid - 1));
-	
-	node -> set_right(buildTree(words, mid + 1, high));
-	
-	return node;
+	return node; // returns the root node of the subtree
 	
 }
--- CS_3305/A6_remDupes/removeDuplicates.cpp
+++ CS_3305/A6_remDupes/removeDuplicates.cpp
@@ -4,48 +4,50 @@
 // Instructor: Dr. Haddad
 // Assignment: 6
 
-#include <cstdlib>
-#include <iostream>
-#include <string>
-#include "bst.h"
+#include <cstdlib>	// Provides the std lib
+#include <iostream> // Provides cout and cin
+#include <string>	// Provides the string type
+#include "bst.h"	// inlcude the bst class and bintree class
 
-using namespace std;
+using namespace std; // allows for all std lib items to be used
 
+// This function is used to print out the words that are 
+// passed to it by the inorder traversal function
 void printWord(string word);
 
 int main() {
 	binary_search_tree<string> dupes;
 	binary_tree_node<string> *root;
 	string input;
-	string temp = "";
+	string temp = ""; // Stores all the chars until the next space
 	
 	while (input != "-1") {
 		cout << "Please input something. Input -1 to end" << endl;
-		getline(cin, input); 
+		getline(cin, input); // takes the entire line as input, allowing for spaces
 		cout << endl;
 		
 		if (input == "-1") {
-			break;
+			break; // exit the loop if input is "-1"
 		}
 		
-		cout << "Original Text: \n" << input << endl;
+		cout << "Original Text: \n" << input << endl; // Prints the entered text
 		cout << endl;
 		
-		for (unsigned int i = 0; i < input.length(); i++) {
-			if (input[i] != ' ') {
-				temp = temp + input[i];
+		for (unsigned int i = 0; i < input.length(); i++) { // iterates through the input 
+			if (input[i] != ' ') { // skips over spaces so that "a" and "a " aren't considered different things
+				temp = temp + input[i]; // add the character to the temp string
 			}
-			if (input[i] == ' ' || i == (input.length() - 1)) {
+			if (input[i] == ' ' || i == (input.length() - 1)) { // puts temp in the BST when you reach a space or the end of the input
 				dupes.insert(temp);
-				temp = "";
+				temp = ""; // clears temp
 			}
 		}
 		
 		cout << "Processed Text: " << endl;
-		inorder(printWord, dupes.get_root());
-		input = "";
-		root = dupes.get_root();
-		tree_clear(root);
+		inorder(printWord, dupes.get_root()); // traverses the BST and prints out the 
+		input = ""; // clears the input
+		root = dupes.get_root(); // gets the root node of the tree
+		tree_clear(root); // clears the tree
 		cout << "\n\n\n" << endl;
 	} 
 	
